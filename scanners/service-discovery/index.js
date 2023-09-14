836641e061fb86c2e9bed23c8b31bb835a06a562
Lila Kelland <lila.kelland@gmail.com>
2023-09-14 16:16:07 -0400

    Using API to connect to database - created key from dns records project names - TODO pull out domains and clean

--- scanners/service-discovery/index.js
+++ scanners/service-discovery/index.js
@@ -4,7 +4,8 @@
 
 // clone dns repo, 
 // extract annotations
-// upsert into database / initate scans 
+// TODO - get domains as well! 
+// upsert into database / send off to endpoint dispatcher
 
 // (from there it will be scanned and added (upserteed into db) and simutaniously services 
 // Something to consider is tracking those repos/ services not captured in dns repo
@@ -13,10 +14,11 @@
 
 // import { tempGetProjects } from "./src/get-projects.js"
 import { cloneDnsRepository, removeClonedDnsRepository} from "./src/clone-dns-repo.js"
-import { insertIntoDatabase } from "./src/database-functions.js";
+import { replaceNonJetstreamCompatibleCharacters, upsertIntoDatabase} from "./src/database-functions.js";
 import { consolidateProjectAnnotations, extractAnnotationsFromDnsRecords } from "./src/extract-project-metadata-from-dns-repo.js";
 import { connect, JSONCodec, jwtAuthenticator } from 'nats'
 import { Database, aql } from "arangojs";
+import { request, gql, GraphQLClient } from 'graphql-request'
 
 import dotenv from 'dotenv'
 // import 'dotenv-safe/config.js'
@@ -27,14 +29,23 @@ const {
   token,
   PORT = 3000,
   HOST = '0.0.0.0',
+  
   DB_NAME = "dataServices",
   // DB_URL = "http://database:8529",
   DB_URL = "http://0.0.0.0:8529",
   DB_USER = "root",
   DB_PASS = 'yourpassword',
-//   NATS_URL = "nats://nats:4222"
-  NATS_URL = "nats://0.0.0.0:4222"
+
+  //   NATS_URL = "nats://nats:4222"
+  NATS_URL = "nats://0.0.0.0:4222",
+  NATS_PUB_STREAM = 'servicesFromDnsRepo',
+  
+  API_URL = "http://0.0.0.0:4000/graphql"
 } = process.env;
+
+// API connection 
+const graphQLClient = new GraphQLClient(API_URL);
+
 
 // Database connection 
 const db = new Database({
@@ -46,12 +57,11 @@ const db = new Database({
 // NATs connection 
 const nc = await connect({ 
   servers: NATS_URL,
-  // authenticator: jwtAuthenticator(jwt), 
 })
 const jc = JSONCodec(); // for encoding NAT's messages
 console.log('🚀 Connected to NATS jetstream server...');
 
-async function publish(subject, payload) {
+async function publish( subject, payload) {
   nc.publish(subject, jc.encode(payload)) 
 }
 
@@ -60,7 +70,7 @@ async function publish(subject, payload) {
 // TODO - pull list from from DB, or nats (from DNS repo)
 
 async function getProjects() {
-    await cloneDnsRepository() 
+    await cloneDnsRepository() //TODO if exisits remove and clone again?
     const dnsRecordsAnnotations = await extractAnnotationsFromDnsRecords();
     const projects = await consolidateProjectAnnotations(dnsRecordsAnnotations);
     // TODO get projects from known-services list, compare as well... concat in
@@ -70,21 +80,20 @@ async function getProjects() {
 
 async function processProjects(projects) {
     for (const project of projects){
-        // TODO - check if exisits first! 
         // TODO Determine what to do with ones in db, but not in this scan
-        const projectKey = await insertIntoDatabase(project, "projects", db) //TODO modify so upsert instead and return key
-        let key = 235 // temp work around 
-        //if not null
-        // await publish(`projects.${key}`, project)
-        
-        console.log("🚀 Sending to scanners...")
+        const upsertService = await upsertIntoDatabase(project, graphQLClient)
+        const serviceKey = upsertService.upsertService._key
+        //TODO check if not undefined
+        await publish(`${NATS_PUB_STREAM}.${serviceKey}`, project)
+
+        console.log(`🚀 Sending to scanners... on ${NATS_PUB_STREAM}.${serviceKey}`)
         console.log(project)
     }  
 } 
 
 const timeout = setTimeout(() => {
     process.exit(0);
-}, 3000);
+}, 5000);
 
 // TODO have this running for one message - or cloud function/ cron job once a day?
 // TODO fix the hanging issue - not able to close (even when running just as function)
@@ -99,7 +108,7 @@ process.on('SIGINT', () => process.exit(0))
   
     await processProjects(projects);
   
-    console.log("Published payload!");
+    // console.log("Published payload!");
     // timeout
     // process.exit(0)
   })();
