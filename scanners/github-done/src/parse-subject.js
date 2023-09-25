f161f0d5f40241a0097f78c52c1d6005cca5e32d
Lila Kelland <lila.kelland@gmail.com>
2023-09-25 11:09:06 -0400

    removed previously named services

--- scanners/github-done/src/parse-subject.js
+++
@@ -1,7 +0,0 @@
-export async function parseSubject(subject) {
-    const parts = subject.split(".").reverse();
-
-    const serviceName = parts[0]; 
-    const checkName = parts[1]; 
-    return { serviceName, checkName }
-}
