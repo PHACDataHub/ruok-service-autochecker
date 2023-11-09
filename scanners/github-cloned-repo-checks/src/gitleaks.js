0fc49c5735bc7414f987471dbdb4a815acff07f3
= <lila.kelland@gmail.com>
2023-11-09 15:41:20 +0000

    added in untested gitleaks

--- scanners/github-cloned-repo-checks/src/gitleaks.js
+++ scanners/github-cloned-repo-checks/src/gitleaks.js
@@ -1,1 +1,35 @@
-gitleaks --path=/path/to/local/repo --depth=-1
+import { CheckOnClonedRepoInterface } from './check-on-cloned-repo-interface.js'
+import { exec } from 'child_process';
+import util from 'util';
+
+const execPromisified = util.promisify(exec);
+
+export async function gitleaksScan(clonedRepoPath) {
+    return new Promise((resolve, reject) => {
+      exec(`gitleaks --path=${clonedRepoPath} --depth=-1`, (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        } else if (stderr) {
+          reject(new Error(stderr));
+        } else {
+          resolve(stdout);
+        }
+      });
+    });
+  }
+
+export class Gitleaks extends CheckOnClonedRepoInterface {
+    constructor(repoName, clonedRepoPath) { 
+        super(repoName, clonedRepoPath); 
+        this.clonedRepoPath = clonedRepoPath;
+        this.repoName = repoName
+    }
+    async doRepoCheck() {
+        const gitleaksResult = await gitleaksScan(this.clonedRepoPath);
+        return {
+            checkPasses: gitleaksResult ,
+            metadata: null,
+            // lastUpdated: Date.now()
+        }
+    }
+}
