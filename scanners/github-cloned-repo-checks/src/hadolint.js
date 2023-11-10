5fd807b36c4a51ffe2a230faf1f0628eefa6d662
= <lila.kelland@gmail.com>
2023-11-10 19:41:58 +0000

    swithcing branches

--- scanners/github-cloned-repo-checks/src/hadolint.js
+++ scanners/github-cloned-repo-checks/src/hadolint.js
@@ -1,1 +1,16 @@
 // https://thenewstack.io/hadolint-lint-dockerfiles-from-the-command-line/#:~:text=is%20called%20Hadolint.-,Hadolint%20is%20a%20command%20line%20tool%20that%20helps%20you%20ensure,tool)%20to%20lint%20the%20code.
+
+//  1. find all dockerfile paths in clonedRepoPath
+//  2. run hadolint for each and apend results
+//  include case no dockerfiles
+//  include case 
+
+// results 
+// checkPasses: true - all dockerfiles have no suggestions
+// metadata {dockerfiles:[paths]}
+// checkPasses: true - all dockerfiles have no suggestions
+// metadata {dockerfiles:[paths]}
+// checkPasses: null - no dockerfile
+// metadata {dockerfiles:[paths]}
+
+
