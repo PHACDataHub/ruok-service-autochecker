e16b2855c4c50c6fba77399409728c5f2f175836
= <lila.kelland@gmail.com>
2023-11-15 19:30:56 +0000

    Added api input type for hadolint

--- api/src/graphql_types/input_types.py
+++ api/src/graphql_types/input_types.py
@@ -32,12 +32,10 @@ class GithubEndpointInput:
     branch_protection: Optional[CheckPassesInput] = None
     has_security_md: Optional[CheckPassesInput] = None
     has_dependabot_yaml: Optional[CheckPassesInput] = None
-    gitleaks: Optional[CheckPassesInput] = None                     
-    hasApiDirectory: Optional[CheckPassesInput] = None                       
-    hasTestsDirectory: Optional[CheckPassesInput] = None                       
-    dotDockerIgnoreDetails: Optional[CheckPassesInput] = None                   
-    dotGitIgnoreDetails: Optional[CheckPassesInput] = None                    
+    gitleaks: Optional[CheckPassesInput] = None       
+    hadolint: Optional[CheckPassesInput] = None   
 
+                
 
 @strawberry.input
 class AccessibilityInput:
