6ce4f17ff22be32b06bb2ae5d23a5fb49db5e3a4
= <lila.kelland@gmail.com>
2024-01-17 15:34:21 +0000

    has-tests-directory tests all pass

--- scanners/github-cloned-repo/src/__tests__/has-tests-directory.test.js
+++ scanners/github-cloned-repo/src/__tests__/has-tests-directory.test.js
@@ -1,82 +1,50 @@
-import { searchForDirectory, searchTests } from '../wipe-later/old-separated-scanners/github-cloned-has-tests-directory-check/src/has-tests-directory.js';
-import * as fs from 'fs';
 
-// TODO use mock-fs https://www.npmjs.com/package/mock-fs
+import { HasTestsDirectory, findTestsPaths } from '../has-tests-directory.js';
+import {  existsSync, rmdirSync } from 'fs';
+import * as fs from 'fs-extra';
 
-describe('searchForDirectory function', () => {
-    let testDirectory;
-  
-    beforeAll(() => {
-        testDirectory = './temp-test-directory';
-        if (!fs.existsSync(testDirectory)) {
-            fs.mkdirSync(testDirectory);
-        }
-  
-      // create subdirectories in the temp directory
-        fs.mkdirSync(`${testDirectory}/dir1`);
-        fs.mkdirSync(`${testDirectory}/dir2`);
-        fs.mkdirSync(`${testDirectory}/dir1/sub1`);
-        fs.mkdirSync(`${testDirectory}/dir2/subdir2`);
-    });
-  
-    afterAll(() => {
-      // clean up
-        if (fs.existsSync(testDirectory)) {
-            fs.rmdirSync(testDirectory, { recursive: true });
-        }
-    });
-  
-    it('should find directories with a given name', () => {
-        const foundDirs = searchForDirectory(testDirectory, 'dir');
-        console.log('****************', foundDirs)
-        expect(foundDirs).toHaveLength(3); 
-        expect(foundDirs).toContain(`${testDirectory}/dir1`); 
-        expect(foundDirs).toContain(`${testDirectory}/dir2`);
-    });
-  
-    it('should return an empty array if no directories match', () => {
-        const foundDirs = searchForDirectory(testDirectory, 'nonexistent');
-    
-        expect(foundDirs).toHaveLength(0); 
-    });
-  });
+import {jest} from '@jest/globals';
 
 
-describe('searchTests function', () => {
+describe('HasTestsDirectory', () => {
   let testDirectory;
-  
-  beforeAll(() => {
-      testDirectory = './temp-test-directory';
-      if (!fs.existsSync(testDirectory)) {
-          fs.mkdirSync(testDirectory);
-      }
 
+  beforeEach(() => {
+    testDirectory = './temp-cloned-repo/temp-repo';
+    fs.ensureDirSync(testDirectory);
+  });
+
+  afterEach(() => {
+    if (existsSync(testDirectory)) {
+      rmdirSync(testDirectory, { recursive: true });
+    }
+  });
+
+  it('should pass if test directories are found', async () => {
+    const repoName = 'test-repo';
     // create subdirectories in the temp directory
-      fs.mkdirSync(`${testDirectory}/dir1`);
-      fs.mkdirSync(`${testDirectory}/dir2`);
-      fs.mkdirSync(`${testDirectory}/dir2/__tests__`);
-      fs.mkdirSync(`${testDirectory}/dir1/sub1`);
-      fs.mkdirSync(`${testDirectory}/dir2/subdir2`);
-      fs.mkdirSync(`${testDirectory}/dir2/subdir2/tests`);
+    fs.ensureDirSync(`${testDirectory}/dir2/__tests__`);
+    fs.ensureDirSync(`${testDirectory}/dir1/sub1`);
+    fs.ensureDirSync(`${testDirectory}/dir2/subdir2/tests`);
+
+    const checker = new HasTestsDirectory(repoName, testDirectory);
+    const result = await checker.doRepoCheck();
+
+    expect(result.checkPasses).toBeTruthy();
+    expect(result.metadata).toEqual({ testDirectoryPaths: ['dir2/__tests__', 'dir2/subdir2/tests'],});
   });
 
-  afterAll(() => {
-      if (fs.existsSync(testDirectory)) {
-          fs.rmdirSync(testDirectory, { recursive: true });
-      }
+  it('should fail if no test directories are found', async () => {
+    const repoName = 'test-repo';
+    // create subdirectories in the temp directory
+    fs.ensureDirSync(`${testDirectory}/dir2`);
+    fs.ensureDirSync(`${testDirectory}/dir1/sub1`);
+    fs.ensureDirSync(`${testDirectory}/dir2/subdir2`);
+
+    const checker = new HasTestsDirectory(repoName, testDirectory);
+    const result = await checker.doRepoCheck();
+
+    expect(result.checkPasses).toBeFalsy();
+    expect(result.metadata).toBeNull();
   });
-
-  it('should return test details for a directory with tests', async () => {
-    const testDetails = await searchTests(testDirectory);
-
-    expect(testDetails).toHaveLength(2);
-    expect(testDetails).toEqual([{"repoScopedPath": "__tests__"}, {"repoScopedPath": "subdir2/tests"}]);
-  });
-
-  it('should return an empty array for a directory without tests', async () => {
-    const testDetails = await searchTests(`${testDirectory}/dir1`);
-
-    expect(testDetails).toHaveLength(0);
-  });
-
 });
