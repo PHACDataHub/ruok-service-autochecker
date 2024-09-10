ebc973e91b34d09ca62e6b098c28def268504117
Siddhartha Haldar <sidhaldar98@gmail.com>
2024-09-09 23:28:31 -0700

    Added uptime data

--- ui/src/components/Service.jsx
+++ ui/src/components/Service.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { Flex, Box, Text, Heading, DataList, Spinner } from '@radix-ui/themes';
+import { Flex, Box, Text, Heading, DataList, Spinner, Separator, Select } from '@radix-ui/themes';
 import { DoubleArrowRightIcon } from '@radix-ui/react-icons';
 import CheckPasses from './CheckPasses';
 import { useQuery, NetworkStatus } from '@apollo/client';
@@ -12,6 +12,8 @@ import {
 
 const Service = () => {
   const { serviceName } = useParams();
+  const [ uptime,setUptime ] = useState([]);
+  const [ uptimeKind,setUptimeKind ] = useState(2);
   const { data, loading, error, networkStatus } = useQuery(
     FETCH_SERVICE_QUERY,
     {
@@ -30,6 +32,49 @@ const Service = () => {
     notifyOnNetworkStatusChange: true,
   });
 
+  const fetchUptime = () => {
+    let doc = '';
+    let data = [];
+    let respTimeMap = { 0 : "", 1 : "-day", 2 : "-week", 3 : "-month", 4 : "-year"};
+    fetch('/uptime',{mode: "cors",referrerPolicy: "unsafe-url"}).then(function (response) {
+        // The API call was successful!
+        return response.text();
+      }).then(function (html) {
+
+        // Convert the HTML string into a document object
+        let parser = new DOMParser();
+        doc =  parser.parseFromString(html, 'text/html');
+        let trs = doc.getElementsByTagName("table")[1].tBodies[0].getElementsByTagName("tr");
+        Array.prototype.slice.call(trs).forEach(tr => {
+            let statusString = tr.children[1].innerText;
+            let serviceName = tr.children[2].innerText.split(/\.y.*ml/)[0];
+            let responseTimes = [];
+            let url = `https://raw.githubusercontent.com/niranjan-ramesh/upptime/master/graphs/${serviceName}/response-time`;
+            for(let idx = 1;idx <= 5;idx++){
+              let responseTime = tr.children[3].getElementsByTagName("a")[idx].firstChild.alt.split(" ").slice(-1)[0]; 
+              responseTimes.push([responseTime,url+respTimeMap[idx-1]+'.png']);
+            }
+            // console.table(statusString,serviceName,responseTimes);
+            data.push([statusString,serviceName,responseTimes]);
+        });
+        // console.log(data);
+        let uptimeData = data.filter(el => el[1] === serviceName);
+        setUptime((prevUptime) => uptimeData);
+      }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+      });
+  }
+
+  useEffect(() => {
+      console.log("inside use effect",uptime);
+      fetchUptime();
+      const interval = setInterval(() => {
+        fetchUptime();
+      }, 5 * 60 * 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   if (networkStatus === NetworkStatus.refetch) return <Spinner />;
   if (loading) return <Spinner />;
   if (error) return <pre>{error.message}</pre>;
@@ -44,6 +89,7 @@ const Service = () => {
   if (!service) {
     return <Text>The service could not be found.</Text>;
   }
+
 
   return (
     <Box style={{ padding: '24px' }}>
@@ -82,12 +128,31 @@ const Service = () => {
               <Trans>Web URL</Trans>
             </DataList.Label>
             <DataList.Value>
-              <Link
-                to={service.webEndpoint.url}
-                style={{ color: 'blue', textDecoration: 'underline' }}
-              >
-                {service.webEndpoint.url}
-              </Link>
+              <Flex gap="3" align="center">
+                <Link
+                  to={service.webEndpoint.url}
+                  style={{ color: 'blue', textDecoration: 'underline' }}
+                >
+                  {service.webEndpoint.url}
+                </Link>
+                <Separator  orientation="vertical"/>
+                {uptime.length > 0 ? uptime[0][0] : "NA"}
+              </Flex>
+            </DataList.Value>
+          </DataList.Item>
+          <DataList.Item>
+            <DataList.Label style={{ minWidth: '150px' }}>
+              <Trans>Github Repo URL</Trans>
+            </DataList.Label>
+            <DataList.Value>
+              <Flex gap="3" align="center">
+                <Link
+                  to={service.githubEndpoint.url}
+                  style={{ color: 'blue', textDecoration: 'underline' }}
+                >
+                  {service.githubEndpoint.url}
+                </Link>
+              </Flex>
             </DataList.Value>
           </DataList.Item>
           <DataList.Item>
@@ -114,6 +179,38 @@ const Service = () => {
             </DataList.Label>
             <DataList.Value>
               {service.githubEndpoint.visibility || 'N/A'}
+            </DataList.Value>
+          </DataList.Item>
+          <DataList.Item>
+            <DataList.Label style={{ minWidth: '150px' }}>
+              <Trans> Response Time</Trans>
+            </DataList.Label>
+            <DataList.Value>
+              <Flex gap="3" align="center">
+                <Link
+                  to="https://niranjan-ramesh.github.io/upptime/"
+                  style={{ color: 'blue', textDecoration: 'underline' }}
+                >
+                  <img height="30" src={uptime.length > 0 ? uptime[0][2][uptimeKind][1] : ""}></img>
+                </Link>
+                <Separator  orientation="vertical"/>
+                <Link
+                  to="https://niranjan-ramesh.github.io/upptime/"
+                  style={{ color: 'blue', textDecoration: 'underline' }}
+                >
+                  {uptime.length > 0 ? uptime[0][2][uptimeKind][0]+'ms' : "NA"}
+                </Link>
+                <Separator  orientation="vertical"/>
+                <Select.Root size="2" defaultValue="2" onValueChange={(e) => setUptimeKind(Number(e))}>
+                  <Select.Trigger />
+                  <Select.Content>
+                    <Select.Item value="1">Day</Select.Item>
+                    <Select.Item value="2">Week</Select.Item>
+                    <Select.Item value="3">Month</Select.Item>
+                    <Select.Item value="4">Year</Select.Item>
+                  </Select.Content>
+                </Select.Root>
+              </Flex>
             </DataList.Value>
           </DataList.Item>
         </DataList.Root>
